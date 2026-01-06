1fcefb9a15986362aa926c9fbb558ce2523d4824
Nayan-Krishna-Ball <nayan.krishna710@gmail.com>
2026-01-06 18:39:11 +0600

    First Upload With Readme file

---
+++ app/[lang]/page.js
@@ -0,0 +1,16 @@
+//
+
+import PhotoList from "@/components/PhotoList";
+
+export default async function Home() {
+  // const dictionary = await getDictionary(lang);
+
+  // console.log(process.env.BASE_API_URL);
+
+  const response = await fetch(`${process.env.BASE_API_URL}/photos`);
+  const photos = await response.json();
+
+  // console.log(photos);
+
+  return <PhotoList photos={photos} />;
+}
