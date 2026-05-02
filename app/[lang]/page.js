83468f83b550e97074f0b29f0186f53839759aa1
Nayan Krishna <nayan.krishna710@gmail.com>
2026-05-03 00:18:21 +0600

    Clean up page.js by removing comments
    Removed commented-out code for clarity.

--- app/[lang]/page.js
+++ app/[lang]/page.js
@@ -1,16 +1,10 @@
-//
 
 import PhotoList from "@/components/PhotoList";
 
 export default async function Home() {
-  // const dictionary = await getDictionary(lang);
-
-  // console.log(process.env.BASE_API_URL);
-
+  
   const response = await fetch(`${process.env.BASE_API_URL}/photos`);
   const photos = await response.json();
-
-  // console.log(photos);
 
   return <PhotoList photos={photos} />;
 }
